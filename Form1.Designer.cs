f6991d58061b4f417b30a43d7c7895a3e53b7a46
Johnson Dinh <52547731+jdinh-782@users.noreply.github.com>
2021-06-09 12:29:13 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.dataTable = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.noteTextBox.Location = new System.Drawing.Point(34, 99);
+            this.noteTextBox.Multiline = true;
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.Size = new System.Drawing.Size(533, 275);
+            this.noteTextBox.TabIndex = 1;
+            this.noteTextBox.Text = "Begin typing...";
+            this.noteTextBox.TextChanged += new System.EventHandler(this.noteTextBox_TextChanged);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.saveButton.Location = new System.Drawing.Point(263, 433);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(112, 37);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.loadButton.Location = new System.Drawing.Point(490, 433);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(112, 37);
+            this.loadButton.TabIndex = 3;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.deleteButton.Location = new System.Drawing.Point(723, 433);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(112, 37);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // dataTable
+            // 
+            this.dataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTable.Location = new System.Drawing.Point(587, 99);
+            this.dataTable.Name = "dataTable";
+            this.dataTable.Size = new System.Drawing.Size(248, 275);
+            this.dataTable.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(587, 59);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(248, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.titleTextBox.Location = new System.Drawing.Point(34, 59);
+            this.titleTextBox.Multiline = true;
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(533, 34);
+            this.titleTextBox.TabIndex = 7;
+            this.titleTextBox.Text = "Title...";
+            this.titleTextBox.TextChanged += new System.EventHandler(this.titleTextBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(34, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 517);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataTable);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.noteTextBox);
+            this.Name = "Form1";
+            this.Text = "Notepad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox noteTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.DataGridView dataTable;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //intiialize a table for the DataTable in the form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("title", typeof(String));
+            table.Columns.Add("notes", typeof(String));
+
+            dataTable.DataSource = table;
+
+            dataTable.Columns["title"].Width = 80;
+            dataTable.Columns["notes"].Width = 125;
+        }
+
+        //save the note when uesr clicks the 'Save' button
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(titleTextBox.Text, noteTextBox.Text);
+
+            newButton_Click(sender, e);
+        }
+
+        private void noteTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void titleTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //delete a certain note when user clicks the 'Delete' button
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (table.Rows.Count >= 1)
+            {
+                int rowIndex = dataTable.CurrentCell.RowIndex;
+
+                table.Rows[rowIndex].Delete();
+            }
+        }
+
+        //load a specific note when user clicks the 'Load' button
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            int rowIndex = dataTable.CurrentCell.RowIndex;
+
+            if (rowIndex >= 0)
+            {
+                titleTextBox.Text = table.Rows[rowIndex].ItemArray[0].ToString();
+                noteTextBox.Text = table.Rows[rowIndex].ItemArray[1].ToString();
+            }
+        }
+
+        //clear both text boxes when user clicks the 'New' button
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Clear();
+            noteTextBox.Clear();
+        }
+    }
+}
